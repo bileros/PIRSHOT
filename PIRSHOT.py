90d323ec557e7dda7790382917b4bfd1bf17504d
bileros <bileros91@gmail.com>
2015-07-08 15:47:43 +0300

    clearance of Pirshot

--- PIRSHOT.py
+++ PIRSHOT.py
@@ -1,18 +1,10 @@
 
-#!/usr/bin/python
-
-# Import required Python libraries
 import RPi.GPIO as GPIO
 import time
 import picamera
-# Use BCM GPIO references
-# instead of physical pin numbers
+
 GPIO.setmode(GPIO.BCM)
-
-# Define GPIO to use on Pi
 GPIO_PIR = 4
-
-# Set pin as input
 GPIO.setup(GPIO_PIR,GPIO.IN)
 
 Current_State  = 0
@@ -20,35 +12,28 @@ Previous_State = 0
 
 try:
 
-  # Loop until PIR output is 0
   while GPIO.input(GPIO_PIR)==1:
     Current_State  = 0
 
   print "  Ready"
 
-  # Loop until users quits with CTRL-C
   while True :
 
-    # Read PIR state
     Current_State = GPIO.input(GPIO_PIR)
 
     if Current_State==1 and Previous_State==0:
-      # PIR is triggered
       print "  Motion detected!"
       with picamera.PiCamera() as camera:
           camera.resolution = (1920, 1080)
           camera.capture('../data/shot.jpg')
-      # Record previous state
       Previous_State=1
     elif Current_State==0 and Previous_State==1:
-      # PIR has returned to ready state
       print "Ready"
       Previous_State=0
 
-    # Wait for 10 milliseconds
     time.sleep(0.01)
 
 except KeyboardInterrupt:
   print "  Quit"
-  # Reset GPIO settings
+
   GPIO.cleanup()
